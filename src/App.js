96070b1f3d4b2936fec6e968d33aa67fb962d580
Omar ATARI <atari.omar@gmail.com>
2020-07-01 17:44:56 +0200

    Add other days call and got temperature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Today from "./Today";
+import Day from "./Day";
 
 const apiUrl = 'https://api.openweathermap.org/data/2.5/'
 const apiKey = '2bc508e7c2711e20c21ba930d4e12dee'
@@ -17,12 +18,20 @@ class App extends Component{
             conditions: '',
             wind: null
         },
+        daystemp: {
+            first: null,
+            second: null,
+            third: null,
+            fourth: null,
+            five: null
+        },
         isLoading: false,
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
         this.getCurrentDayData(this.city.value)
+        this.getFuturDaysData(this.city.value).then(() => { console.log(this.state.daystemp) })
     }
 
     async getCurrentDayData(city){
@@ -37,6 +46,25 @@ class App extends Component{
                 conditions: data.weather[0].description,
                 temp: data.main.temp,
                 wind: data.wind.speed
+            },
+            isLoading: false
+        })
+    }
+
+    async getFuturDaysData(city){
+        this.setState({
+            isLoading: true,
+        })
+        const call = await fetch(`${apiUrl}forecast?q=${city}&APPID=${apiKey}&lang=fr&units=metric`)
+        const data = await call.json()
+
+        this.setState({
+            daystemp: {
+              first: data.list[0].main.temp,
+              second: data.list[1].main.temp,
+              third: data.list[2].main.temp,
+              fourth: data.list[3].main.temp,
+              five: data.list[4].main.temp
             },
             isLoading: false
         })
@@ -57,6 +85,11 @@ class App extends Component{
                 wind={this.state.today.wind}
                 conditions={this.state.today.conditions}
                 />
+                <Day title="Day +1 " temperature={this.state.daystemp.first}/>
+                <Day title="Day +2 " temperature={this.state.daystemp.second}/>
+                <Day title="Day +3 " temperature={this.state.daystemp.third}/>
+                <Day title="Day +4 " temperature={this.state.daystemp.fourth}/>
+                <Day title="Day +5 " temperature={this.state.daystemp.five}/>
             </>
         )
     }
